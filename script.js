6c345a72bde8568e22938358afd0777a848992d1
AlamSirji <alamsirji@pm.me>
2021-05-29 02:18:42 +0530

    Updated Randomization

--- script.js
+++ script.js
@@ -25,7 +25,6 @@ class gradient {
         var colorStops = [];
         var degree;
         var percentages = [];
-        var gradientType = Math.floor(Math.random() * (1 - 0 + 1) + 0);
         var totalColorStops = Math.floor(Math.random() * (maxColorStop - minColorStop + 1) + minColorStop);
         for (let index = 0; index < totalColorStops; index++) {
             colorStops.push(this.getRandomColor());
@@ -37,41 +36,41 @@ class gradient {
             percentages.push(Math.floor(Math.random() * (100 - 0 + 1) + 0) + "%");
         }
 
-        if (gradientType === 1) {
+        if (Math.round(Math.random()*10 >= 2)) {
             // linear gradient
             switch (totalColorStops) {
                 case 3:
                     cssStyleValue = `linear-gradient(
                             ${degree}deg, 
-                            #${colorStops[0]} ${percentages[0]}, 
-                            #${colorStops[1]} ${percentages[1]}, 
-                            #${colorStops[2]} ${percentages[2]})`;
+                            ${colorStops[0]} ${percentages[0]}, 
+                            ${colorStops[1]} ${percentages[1]}, 
+                            ${colorStops[2]} ${percentages[2]})`;
                     break;
 
                 case 4:
                     cssStyleValue = `linear-gradient(
                             ${degree}deg, 
-                            #${colorStops[0]} ${percentages[0]}, 
-                            #${colorStops[1]} ${percentages[1]},
-                            #${colorStops[2]} ${percentages[2]}, 
-                            #${colorStops[3]} ${percentages[3]})`;
+                            ${colorStops[0]} ${percentages[0]}, 
+                            ${colorStops[1]} ${percentages[1]},
+                            ${colorStops[2]} ${percentages[2]}, 
+                            ${colorStops[3]} ${percentages[3]})`;
                     break;
 
                 case 5:
                     cssStyleValue = `linear-gradient(
                             ${degree}deg, 
-                            #${colorStops[0]} ${percentages[0]}, 
-                            #${colorStops[1]} ${percentages[1]},
-                            #${colorStops[2]} ${percentages[2]}, 
-                            #${colorStops[3]} ${percentages[3]}, 
-                            #${colorStops[4]} ${percentages[4]})`;
+                            ${colorStops[0]} ${percentages[0]}, 
+                            ${colorStops[1]} ${percentages[1]},
+                            ${colorStops[2]} ${percentages[2]}, 
+                            ${colorStops[3]} ${percentages[3]}, 
+                            ${colorStops[4]} ${percentages[4]})`;
                     break;
 
                 default:
                     cssStyleValue = `linear-gradient(
                             ${degree}deg, 
-                            #${colorStops[0]} ${percentages[0]}, 
-                            #${colorStops[1]} ${percentages[1]})`;
+                            ${colorStops[0]} ${percentages[0]}, 
+                            ${colorStops[1]} ${percentages[1]})`;
                     break;
             }
 
@@ -87,30 +86,30 @@ class gradient {
                 // by shape only
                 cssStyleValue = `radial-gradient(
                         ${shape} at ${percentages[0]}, 
-                        #${colorStops[0]} ${percentages[1]}, 
-                        #${colorStops[1]} ${percentages[2]})`;
+                        ${colorStops[0]} ${percentages[1]}, 
+                        ${colorStops[1]} ${percentages[2]})`;
             } else {
                 // by size only
                 cssStyleValue = `radial-gradient(
                         ${size} at ${percentages[0]} ${percentages[1]}, 
-                        #${colorStops[0]} ${percentages[2]}, 
-                        #${colorStops[1]} ${percentages[3]})`;
+                        ${colorStops[0]} ${percentages[2]}, 
+                        ${colorStops[1]} ${percentages[3]})`;
             }
         }
-
         return cssStyleValue;
     }
 
     getRandomColor() {
-        const indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
-        const code = [];
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
-        return code.join("");
+        if (Math.round(Math.random()*10) <= 2) {
+            return 'transparent'
+        } else {
+            const indices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+            const code = [];
+            for (let index = 0; index < 6; index++) {
+                code.push(indices[Math.floor(Math.random() * (indices.length - 0))]);
+            }
+            return "#" + code.join("");
+        }
 
     }
 
@@ -123,8 +122,12 @@ class gradient {
     printLogs(cssStyleValue) {
         const logOL = document.querySelector("#logContainer ol");
         const newLi = document.createElement("LI");
-        newLi.innerHTML = `background-image: ${cssStyleValue};`;
-        newLi.setAttribute("data-gradient-code", `${cssStyleValue};`);
+        newLi.innerHTML = `
+                background - image: ${cssStyleValue};
+                `;
+        newLi.setAttribute("data-gradient-code", `
+                ${cssStyleValue};
+                `);
         newLi.setAttribute("title", "Click to Copy");
         logOL.appendChild(newLi);
         logOL.scrollTo(0, logOL.scrollHeight);
@@ -132,21 +135,21 @@ class gradient {
             // apply style to current body
             myGradient.container.style.backgroundImage = cssStyleValue;
         });
-        // newLi.addEventListener("click", (e) => {
-        //     let cssStyleValue = e.target.innerHTML;
-        //     // copy style to clipboard
-        //     if (typeof Navigator.clipboard != undefined) {
-        //         const textArea = document.createElement("TEXTAREA");
-        //         textArea.value = cssStyleValue;
-        //         document.body.appendChild(textArea);
-        //         textArea.select();
-        //         document.execCommand("copy");
-        //         textArea.remove();
-        //     } else {
-        //         Navigator.clipboard.writeText(cssStyleValue);
-        //     }
-        //     myGradient.container.style.backgroundImage = cssStyleValue;
-        // })
+        newLi.addEventListener("click", (e) => {
+            let cssStyleValue = e.target.innerHTML;
+            // copy style to clipboard
+            if (typeof Navigator.clipboard != undefined) {
+                const textArea = document.createElement("TEXTAREA");
+                textArea.value = cssStyleValue;
+                document.body.appendChild(textArea);
+                textArea.select();
+                document.execCommand("copy");
+                textArea.remove();
+            } else {
+                Navigator.clipboard.writeText(cssStyleValue);
+            }
+            myGradient.container.style.backgroundImage = cssStyleValue;
+        })
         newLi.addEventListener("touchstart", (e) => {
             // apply style to current body
             myGradient.container.style.backgroundImage = cssStyleValue;
@@ -168,7 +171,9 @@ window.addEventListener("keypress", (e) => {
 function changeView(e) {
     const ids = ["logContainer"];
     for (let index = 0; index < ids.length; index++) {
-        const e = document.getElementById(`${ids[index]}`);
+        const e = document.getElementById(`
+                $ { ids[index] }
+                `);
         if (e.classList.contains("hide")) {
             e.classList.remove("hide");
         } else {
